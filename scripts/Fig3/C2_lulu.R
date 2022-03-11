1ef887211016d5d7d9d5e2594dbffb760380a6e3
MiguelMSandin <miguelmendezsandin@gmail.com>
2022-03-11 17:00:31 +0100

    Update and rename lulu.R to C2_lulu.R

--- scripts/Fig3/C2_lulu.R
+++ scripts/Fig3/C2_lulu.R
@@ -2,7 +2,10 @@
 library(lulu)
 library(data.table)
 
-setwd("/home/mmsandin/Desktop/PhD/0_Thesis/3.1_Chapter/github/data/illumina/lulu/")
+setwd("data/illumina/lulu/")
+
+system("cp ../dada2/default_pipeline/out/ASVs_taxo.fasta .")
+system("vsearch --threads 2 --usearch_global ASVs_taxo.fasta --db ASVs_taxo.fasta --self --id .84 --iddef 1 --userout match_list.txt -userfields query+target+id --maxaccepts 0 --query_cov .9 --maxhits 10")
 
 abun <- fread("ASVs_abun.tsv", sep="\t")
 simm <- fread("match_list.txt", sep="\t")
